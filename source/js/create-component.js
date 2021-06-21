06269b6aab65a53b5ad4f9ab7b7ac4282895e736
Roman <demromdeveloper@gmail.com>
2021-06-21 19:41:22 +0300

    wip(all): finish the modul of creating a components flat array

--- source/js/create-component.js
+++ source/js/create-component.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-console */
 'use strict';
 
-// FIXME общие переменные вынести в main.js перед конкантенацией
-
-// import {groups} from './groups.min.js';
-// import {flatArray} from './flat-array.min.js';
-// import {checkForm} from './form-validating.min.js';
-
-// const MAIN_ROOT_LIST = 'ul[id="0"]';
-// let mainRootList = document.querySelector(MAIN_ROOT_LIST);
+const MAIN_ROOT_LIST = 'ul[id="0"]';
+let mainRootList = document.querySelector(MAIN_ROOT_LIST);
 // console.log(mainRootList);
 
 // data атрибуты
--- source/js/flat-array.js
+++ source/js/flat-array.js
@@ -3,79 +3,80 @@
 
 const flatArray = [
   {
-    parent: 0,
+    parentId: 0,
     id: 1,
     name: 'Дети',
+    children: true,
   },
   {
-    parent: 0,
+    parentId: 0,
     id: 13,
     name: 'Профессиональные сообщества',
   },
   {
-    parent: 0,
+    parentId: 0,
     id: 14,
     name: 'Местные сообщества',
   },
   {
-    parent: 0,
+    parentId: 0,
     id: 15,
     name: 'Религиозные общины',
   },
   {
-    parent: 1,
+    parentId: 1,
     id: 2,
     name: 'Имеющие редкие заболевания',
   },
   {
-    parent: 1,
+    parentId: 1,
     id: 6,
     name: 'Имеющие тяжелые заболевания',
   },
   {
-    parent: 1,
+    parentId: 1,
     id: 10,
     name: 'С инвалидностью',
   },
   {
-    parent: 1,
+    parentId: 1,
     id: 11,
     name: 'ВИЧ-положительные',
   },
   {
-    parent: 1,
+    parentId: 1,
     id: 12,
     name: 'Мигранты и беженцы',
   },
   {
-    parent: 6,
-    id: 7,
-    name: 'Синдром Лежена',
-  },
-  {
-    parent: 6,
-    id: 8,
-    name: 'Синдром Ретта',
-  },
-  {
-    parent: 6,
-    id: 9,
-    name: 'Эктодермальная ангидротическая дисплазия',
-  },
-  {
-    parent: 2,
+    parentId: 2,
     id: 3,
     name: 'Spina Bifida',
   },
   {
-    parent: 2,
+    parentId: 2,
     id: 4,
     name: 'Буллёзный эпидермолиз',
   },
   {
-    parent: 2,
+    parentId: 2,
     id: 5,
     name: 'Гемофилия',
+  },
+  {
+    parentId: 6,
+    id: 7,
+    name: 'Синдром Лежена',
+  },
+  {
+    parentId: 6,
+    id: 8,
+    name: 'Синдром Ретта',
+  },
+  {
+    parentId: 6,
+    id: 9,
+    name: 'Эктодермальная ангидротическая дисплазия',
   },
 ];
 
